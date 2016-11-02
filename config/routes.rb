c09e6dbed6ad406082cd437bfba1d3ef38766db2
Naman Shah <naman@debian>
2016-11-02 03:40:39 -0400

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'establishments#index'
+  root 'home#index'
 
   resources :establishments
 
