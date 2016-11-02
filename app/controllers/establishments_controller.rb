d4b9821d9e65469e796a9cb1dbcfeecd6dcc0bc7
Naman Shah <naman@debian>
2016-11-01 23:39:02 -0400

    Added edit and update actions for Establishment Table

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -11,11 +11,9 @@ class EstablishmentsController < ApplicationController
 		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
 		firebase = Firebase::Client.new(base_uri)
 
-		establishment = firebase.get('/establishment/' + params[:id]).body
-		@establishment = {params[:id] => establishment}
+		@establishment = firebase.get('/establishment/' + params[:id]).body
 
-		manager = firebase.get('/manager/' + establishment["MID"].to_s).body
-		@manager = {establishment["MID"] => manager}
+		@manager = firebase.get('/manager/' + @establishment["MID"].to_s).body
 
 		# TODO: REPLACE THIS BLOCK AFTER LEARNING ABOUT FIREBASE-RUBY QUERY OPTIONS
 		reports = firebase.get('/report/').body
@@ -33,17 +31,17 @@ class EstablishmentsController < ApplicationController
 		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
 		firebase = Firebase::Client.new(base_uri)
 
-		id = firebase.get('/establishment/Incrementor').body
-		m_id = firebase.get('/manager/Incrementor').body
+		new_id = firebase.get('/establishment/Incrementor').body
+		new_m_id = firebase.get('/manager/Incrementor').body
 
-		add_manager = firebase.set('/manager/' + m_id.to_s, {
+		add_manager = firebase.set('/manager/' + new_m_id.to_s, {
 				"Name" => params["Manager Name"],
 				"Phone" => params["Manager Phone"],
 				"Email" => params["Manager Email"]
 			})
-		firebase.update('', {'manager/Incrementor' => m_id + 1})
+		firebase.update('', {'/manager/Incrementor' => new_m_id + 1})
 
-		add_establishment = firebase.set('/establishment/' + id.to_s, {
+		add_establishment = firebase.set('/establishment/' + new_id.to_s, {
 				"Name" => params["Name"],
 				"Location" => {
 						"Address" => params["Address"],
@@ -51,11 +49,46 @@ class EstablishmentsController < ApplicationController
 						"State" => params["State"],
 						"Zip" => params["Zip"]
 					},
-				"MID" => m_id,
+				"MID" => new_m_id,
 				"Status" => params["Status"]
 			})
-		firebase.update('', {'establishment/Incrementor' => id + 1})
+		firebase.update('', {'/establishment/Incrementor' => new_id + 1})
 
 		redirect_to establishments_path
+	end
+
+	def edit
+		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
+		firebase = Firebase::Client.new(base_uri)
+
+		@establishment = firebase.get('/establishment/' + params[:id]).body
+
+		@manager = firebase.get('/manager/' + @establishment["MID"].to_s).body
+	end
+
+	def update
+		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
+		firebase = Firebase::Client.new(base_uri)
+
+		establishment = firebase.get('/establishment/' + params[:id]).body
+
+		update_manager = firebase.update('/manager/' + establishment["MID"].to_s, {
+				"Name" => params["Manager Name"],
+				"Phone" => params["Manager Phone"],
+				"Email" => params["Manager Email"]
+			})
+
+		update_establishment = firebase.update('/establishment/' + params[:id], {
+				"Name" => params["Name"],
+				"Location" => {
+					"Address" => params["Address"],
+					"City" => params["City"],
+					"State" => params["State"],
+					"Zip" => params["Zip"]
+				},
+				"Status" => params["Status"]
+			})
+
+		redirect_to establishment_path(params[:id])
 	end
 end
