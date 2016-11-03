5793be253d384e950adb50207b73954880dd9a6d
Naman Shah <naman@debian>
2016-11-03 15:57:26 -0400

    Added edit and update actions for Reports Table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,4 +17,32 @@ class ReportsController < ApplicationController
 
 		#@volunteer = firebase.get('/volunteer/' + @report["VID"].to_s).body
 	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
+		firebase = Firebase::Client.new(base_uri)
+
+		@report = firebase.get('/report/' + params[:id]).body
+	end
+
+	def update
+		base_uri = 'https://hb141-2fc0d.firebaseio.com/'
+		firebase = Firebase::Client.new(base_uri)
+
+		report = firebase.get('/report/' + params[:id]).body
+
+		update_report = firebase.update('/report/' + params[:id], {
+				"Datetime" => params["Datetime"],
+				"Comment" => params["Comment"],
+				"Pass" => params["Pass"]
+			})
+
+		redirect_to report_path(params[:id])
+	end
 end
